936c74a77f4425aa4494b78dd2722d0d172f84da
Sam Phan <nsphan1207@gmail.com>
2024-11-27 15:15:12 -0800

    feat: added blog objects and list of blogs

---
+++ src/blog.ts
@@ -0,0 +1,28 @@
+
+type Blog = {
+    title:string;
+    date:string;
+    description:string;
+    image:string;
+    imageAlt:string;
+    slug:string;
+};
+
+const blogs: Blog[] = [
+    {
+        title:"First Blog",
+        date:"11-26-2024",
+        description:"This is my first blog.",
+        image:"./pk_ball.png",
+        imageAlt: "Pokeball Placeholder",
+        slug: "first-blog",
+    },
+    {
+        title:"Second Blog",
+        date:"11-27-2024",
+        description:"This is my second blog.",
+        image:"./pk_ball.png",
+        imageAlt: "Pokeball Placeholder pt 2!",
+        slug: "second-blog",
+    }
+];
