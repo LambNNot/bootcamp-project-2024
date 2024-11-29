329f59ac2eeff10c47615a075ea564207b080442
Sam Phan <nsphan1207@gmail.com>
2024-11-28 21:24:51 -0800

    feat: Copy over blogs data into blogData.ts

---
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -0,0 +1,57 @@
+
+type Blog = {
+    title:string;
+    date:string;
+    description:string;
+    image:string;
+    imageAlt:string;
+    slug:string;
+};
+
+const blogs: Blog[] = [
+    {
+        title:"First Blog",
+        date:"11-26-2024",
+        description:"This is my first blog.",
+        image:"./pk_ball.png",
+        imageAlt: "Pokeball Placeholder",
+        slug: "first-blog",
+    },
+    {
+        title:"Second Blog",
+        date:"11-27-2024",
+        description:"This is my second blog.",
+        image:"./pk_ball.png",
+        imageAlt: "Pokeball Placeholder pt 2!",
+        slug: "second-blog",
+    }
+];
+
+//Reference blogs container
+const blogsContainer = document.getElementById("blogs-container")
+
+//Traverse through blogs list, appending to blogs container
+blogs.forEach(blog=>{
+    const blogLink = document.createElement("a");
+    blogLink.setAttribute("href", "./blogs/"+blog.slug+".html");
+    blogsContainer?.appendChild(blogLink);
+
+    const blogPostContainer = document.createElement("div");
+    blogPostContainer.classList.add("blog-post");
+
+    const blogTitle = document.createElement("h1");
+    blogTitle.textContent = blog.title;
+    blogTitle.classList.add("blog-title");
+    blogPostContainer.appendChild(blogTitle);       //Add blog title
+
+    const blogImg = document.createElement("img");
+    blogImg.src = blog.image;
+    blogImg.style.cssText = "width:50px; height:50px;";
+    blogPostContainer.appendChild(blogImg);         //Add blog image
+
+    const blogDesc = document.createElement("p");
+    blogDesc.textContent = blog.description;
+    blogPostContainer.appendChild(blogDesc);        //Add blog description
+
+    blogLink.appendChild(blogPostContainer); //Add blog post to blogs container
+});
