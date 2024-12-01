6df121c356ff7fab085b15695be8a7e90ed314b4
Sam Phan <nsphan1207@gmail.com>
2024-11-30 17:23:24 -0800

    refactor: Changed @ alias from src/ to src/app/

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import {Inter} from "next/font/google";
 import "./globals.css";
-import NavBar from "@/app/components/navbar";
-import Footer from "@/app/components/footer";
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({subsets: ["latin"]});
 
