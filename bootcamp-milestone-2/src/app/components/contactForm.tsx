e2940ad2117cee78c553bc0f26d3b72d4ad0b89f
Sam Phan <nsphan1207@gmail.com>
2024-12-22 04:32:16 -0800

    feat: Add contact me page functionality

---
+++ bootcamp-milestone-2/src/app/components/contactForm.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { Reference, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "@/globals.css";
+import style from "@/components/contactForm.module.css";
+
+export default function contactForm(){
+    const form = useRef<HTMLFormElement>(null);
+
+    const sendEmail = (event:React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+
+        if(form.current == null){
+            console.error("Form ref null");
+            return;
+        }
+
+        emailjs.sendForm(
+            "service_43usvga", // Service ID
+            "template_kpuja7f", // Template ID
+            form.current, // Form
+            "_Fc2Th3LQBHyb13np", //Public Key
+        ).then(
+            (result) => {
+                console.log("Success: ", result.text);
+                alert("Message sent!");
+            },(error) => {
+                console.error("Error: ", error.text);
+                alert("Message could not be sent.");
+            }
+        );
+
+        form.current.reset(); // Reset form fields
+    }
+    return(
+        <>
+        <form className={style.form} ref={form} onSubmit={sendEmail}> {/*Set up eventHandler*/}
+            <input className={style.field} type="text" name="user_name" placeholder="Your Name" required />
+            <input className={style.field} type="email" name="user_email" placeholder="Your Email" required />
+            <textarea className={style.field} name="message" placeholder="Your Message" required />
+            <button type="submit">Send Message</button>
+        </form>
+        </>
+    )
+}
--- bootcamp-milestone-2/src/app/contact/page.tsx
+++ bootcamp-milestone-2/src/app/contact/page.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import style from "./contact.module.css";
 import "../globals.css";
+import ContactForm from "@/components/contactForm";
 
 export default function Contact(){
     return(
         <>
         <main>
             <h1 className="mainTitle">Contact</h1>
-            <form className={style.contactForm}>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" required/>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" required/>
-                <label htmlFor="message">Message</label>
-                <textarea required>
-
-                </textarea>
-                <input className={style.contactButton} type="submit" id="submit-button"/>
-            </form>
+            <ContactForm/>
         </main>
         </>
     );
