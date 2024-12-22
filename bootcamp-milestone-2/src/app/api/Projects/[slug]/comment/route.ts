290b29fbfb6414703a25629fce71282a6864729b
Sam Phan <nsphan1207@gmail.com>
2024-12-21 23:31:30 -0800

    feat: Add dynamic API endpoints for portfolio project page commenting

---
+++ bootcamp-milestone-2/src/app/api/Projects/[slug]/comment/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/../database/db";
+import projectSchema from "@/../database/projectSchema"
+import {IComment} from "@/../database/comment";
+
+type IParams = {
+    params: {
+        slug: string;
+    }
+}
+
+export async function POST(req:NextRequest, {params}:IParams){
+    await connectDB();
+
+    const {slug} = await params; // Get slug
+    const body:IComment = await req.json();
+
+    if (!body.user) {
+		return NextResponse.json('Comment has no valid user', {status:400});
+	}else if(!body.comment){
+        return NextResponse.json('Invalid comment', {status:400});
+    }
+
+    try{
+        const project = await projectSchema.findOneAndUpdate(
+            {slug}, // Filter
+            {$push:{ 
+                // Push new comment
+                comments:{
+                    user: body.user,
+                    comment: body.comment,
+                    time: body.time
+                }
+            }},
+            {new:true, runValidators:true}
+        ).orFail();
+        return NextResponse.json(project);
+    }catch(err){
+        return NextResponse.json('Project not found.', {status : 404});
+    }
+}
